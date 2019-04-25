e3f2a98430b28bfd4d37bd0ae1f8067bccd012d3
medusa <medusa@transfer-digitize.library.illinois.edu>
2019-04-25 15:47:14 -0400

    Add some echo statements and add -d option to docker-compose up

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-source ./env.sh
+echo "Sourcing env.sh"
+source $HOME/bin/env.sh
 
 #coordinate this with the docker-compose file to make
 #sure that the right volumes exist
+echo "Ensure log and run directories exist"
 instances=(1 2 3 4)
 for i in "${instances[@]}"; do
     mkdir -p "$RUN_DIR/$i"
     mkdir -p "$LOG_DIR/$i"
 done
 
+echo "Running docker-compose"
 cd $COPIER_DIR
-docker-compose start || docker-compose up
+docker-compose start || docker-compose up -d
